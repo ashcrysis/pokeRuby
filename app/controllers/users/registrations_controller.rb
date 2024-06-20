1b9cd920c0ee770e3a29948f4454359b25374fbf
Ashen Silva <109311931+ashcrysis@users.noreply.github.com>
2024-06-20 08:36:02 -0300

    Update app/controllers/users/registrations_controller.rb
    Co-authored-by: pbressan13 <49291866+pbressan13@users.noreply.github.com>

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,15 +4,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionsFix
   respond_to :json
 
-  def respond_with(current_user, _opts = {})
-    if resource.persisted?
+before_action :configure_sign_up_params, only: [:create]
+
+  def create
+    @user = User.new(sign_up_params)
+
+    if @user.save
       render json: {
-        status: {code: 200, message: 'Signed up successfully.'},
-        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+        status: { code: 200, message: 'Signed up successfully.' },
+        data: UserSerializer.new(@user).serializable_hash[:data][:attributes]
       }
     else
       render json: {
-        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+        status: { message: "User couldn't be created successfully. " + @user.errors.full_messages.to_sentence }
       }, status: :unprocessable_entity
     end
   end
