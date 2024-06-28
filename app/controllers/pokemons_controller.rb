efb9a8584a162dea44b039d8ab5822267f6ca92e
Pedro Bressan <pbressan13@gmail.com>
2024-06-28 11:47:39 -0300

    Structures to repositories and presenters

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -35,7 +35,7 @@ class PokemonsController < ApplicationController
   end
 
   def fetch_all_pokemon_data
-    response = pokemons_service.fetch_pokemon
+    response = pokemons_service.all_pokemons
     render json: response
   end
 
@@ -88,6 +88,6 @@ class PokemonsController < ApplicationController
   end
 
   def pokemons_service
-    PokemonsService
+    PokemonsService.new(PokemonsApiRepository.new)
   end
 end
--- app/services/pokemons_service.rb
+++ app/services/pokemons_service.rb
@@ -1,38 +1,11 @@
 class PokemonsService
-  POKEMON_API = "https://pokeapi.co/api/v2/pokemon"
-  API = false 
 
-  def self.fetch_pokemon
-    if API 
-      fetch_all_pokemon_data_from_url
-    else 
-      fetch_all_pokemon_data_from_db
-    end
+  def initialize(repository = PokemonsRepository.new)
+    @repository = repository
   end
 
-  private
+  def all_pokemons
+    JSON.parse(@repository.fetch_all_as_json)
+  end
 
-  def self.fetch_all_pokemon_data_from_url
-    response = HTTParty.get("#{POKEMON_API}?limit=10000")
-    if response.code == 200
-      JSON.parse(response.body)
-    end
-  end 
-
-  def self.fetch_all_pokemon_data_from_db
-    pokemons = Pokemon.all
-    count = pokemons.count
-    pokemon_data = pokemons.map do |pokemon|
-      {
-        "name" => pokemon.nome,
-        "url" => pokemon.imagem
-      }
-    end
-    {
-      "count" => count,
-      "next" => nil,
-      "previous" => nil,
-      "results" => pokemon_data
-    }
-  end 
 end
