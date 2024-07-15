8e06d1bd36d3e1b7f658b2bb0d140077d7ff3219
ashcrysis <arincommissions@gmail.com>
2024-07-15 13:19:46 -0300

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,11 +25,15 @@ module PokeRuby
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000' ,'http://35.173.15.73:3000/'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true,
-        expose: ['Authorization']
+        origins 'http://localhost:3000', 'http://35.173.15.73:3000'
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          expose: ['Authorization'],
+          max_age: 600
       end
-  end
+    end
 
   end
 end
