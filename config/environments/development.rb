4eafc415f6d7ae0b87788ff7655701c62f8a441a
ashcrysis <arincommissions@gmail.com>
2024-07-15 08:41:18 -0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+  config.action_mailer.default_url_options = { host: '3.235.215.243', port: 3001 }
 end
